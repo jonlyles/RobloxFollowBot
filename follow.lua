04c272eda9e65be50a3b806145e824156a3c5099
Jon Lyles <confidential99@gmail.com>
2012-05-13 16:29:18 -0400

    findNearestTorso function split into smaller chunks

--- follow.lua
+++ follow.lua
@@ -1,36 +1,76 @@
 -- Forked by TheEagleGuy from BontyHunter4567
 
-function findNearestTorso(pos)
-    local list = game.Workspace:children()
-    local torso = nil
-    local distance = 10
-    local temp = nil
-    local human = nil
-    local temp2 = nil
-    for x = 1, #list do
-        temp2 = list[x]
-        if (temp2.className == "Model") and (temp2 ~= script.Parent) then
-            temp = temp2:findFirstChild("Torso")
-            human = temp2:findFirstChild("Humanoid")
-            if (temp ~= nil) and (human ~= nil) and (human.Health > 0) then
-                if (temp.Position - pos).magnitude < distance then
-                    torso = temp
-                    dist = (temp.Position - pos).magnitude
-                    end 
-                end 
+-- Finds all Humanoids in SearchArea
+-- Returns HumanoidsList, a table of all Humanoids in the SearchArea
+function GetHumanoids( SearchArea )
+    -- Default SearchArea is Workspace
+    SearchArea = SearchArea or game.Workspace      
+    -- Check all children of SearchArea
+    for key,val in pairs(SearchArea:GetChildren()) do
+        -- Check if its ClassName is Model
+        if val.ClassName == "Model" then
+            for k,v in pairs(val:GetChildren()) do
+               -- Check if its ClassName is Humanoid.
+               if v.ClassName == "Humanoid" then
+                   -- If ClassName is Humanoid, add it to PlayerList
+                   table.insert(HumanoidsList,val)
+               end
             end 
+        end
+    end
+    return HumanoidsList
+end
+
+
+-- Takes a table of Humanoids and removes self (script.Parent), and 
+-- removes any Humanoid that is dead ( Health == 0 )
+-- @param HumanoidsList, table containing 
+-- @return HumanoidsList, table (clean)
+function CleanHumanoids( HumanoidsList )
+    assert( Humanoids.className == 'Humanoid' )
+    local HumanoidsClean = {}
+    for x = 1, #HumanoidsList do
+        if HumanoidsList[x] ~= script.Parent and HumanoidsList[x].Health ~= 0 then
+            table.insert( HumanoidsClean, HumanoidsList[x] )
+        end
+    end
+    return HumanoidsList = HumanoidsClean
+end
+
+
+-- Takes a table of Humanoids and determines which is closest
+-- @param pos, Vector3 Position 
+-- @param HumanoidsList, table containing Humanoids
+-- @return NearestHumanoid
+function findNearestHumanoid( pos, HumanoidsList )
+    assert( type(HumanoidsList) == 'table' )
+    local NearestHumanoid = nil
+    local candidate = nil
+    local distance = 10
+        
+    for x = 1, #HumanoidsList do
+        candidate = HumanoidsList[x]
+        if (candidate.Position - pos).magnitude < distance then
+            NearestHumanoid = candidate
+            distance = (candidate.Position - pos).magnitude  -- what is the purpose of this line?
         end 
-    return torso 
+    end 
+    return NearestHumanoid 
 end 
 
-while wait(.01) do
-    local target = findNearestTorso(script.Parent.Torso.Position)
-    if target ~= nil then 
-        if (script.Parent:findFirstChild("ForceField")) or (target.Parent:findFirstChild("ForceField")) or (target.Position.Y < script.Parent["Right Leg"].Position.Y) then 
+-- Follows a target
+-- @param NearestHumanoid, this is the target
+-- @param Follower, this is who will follow
+-- @return returns a callback, where to move the follower
+function FollowTarget(NearestHumanoid,Follower)
+    assert( NearestHumanoid ~= nil and Follower ~= nil )
+    
+    -- what is the purpose of this?
+    if ( target.Parent:findFirstChild("ForceField")) or (target.Parent:findFirstChild("ForceField")) or (target.Position.Y < script.Parent["Right Leg"].Position.Y) then 
         else 
             script.Parent.Humanoid:MoveTo(target.Position + Vector3.new(math.random(1,3),0,math.random(1,3)), target)
             wait(.15) 
             script.Parent.Humanoid:MoveTo(target.Position - Vector3.new(math.random(1,3),0,math.random(1,3)), target)
         end 
     end 
-end 
+end
